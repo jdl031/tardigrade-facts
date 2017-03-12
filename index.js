659fb6ea7d2762c40e96950c7b7f700e08a5427e
James Lloyd <james.jd.lloyd@gmail.com>
2017-03-11 19:54:00 -0600

    fix up

--- index.js
+++ index.js
@@ -13,44 +13,20 @@ app.listen(port, () => {
 });
 
 function handleCall(req,res){
+
   const slackToken = process.env.SLACK_TOKEN;
   const channel = req.body.channel_id;
 
-  if (!slackToken) {
-    return res.status(400).send('tardigrade-facts has not been properly configured');
-  }
-
-// replace logic here.
-
-  getFact(function(err, factData , slackToken){
-    if (err){
-      return res.status(400).send('There was a problem. The Tardigrades are on it.');
-    }
-    res.status(200).send('Tardigrade Fact' + factData);
-  })
-
-  function getFact(cb){
-    var facts = [
-      "Tardigrades can survive being frozen for over 32 years",
-      "Tardigrades can survive in space",
-      "Tardigrades can survive 570,000 Rontgens. Humans can only survive 500."
-    ]
+  const facts = [
+    "Tardigrades can survive being frozen for over 32 years",
+    "Tardigrades can survive in space",
+    "Tardigrades can survive 570,000 Rontgens. Humans can only survive 500.",
+    "More than 1,150 species of Tardigrades have been identified so far since 1778.",
+    "Tardigrades are usually 0.5 mm long but they can grow a little longer and reach up to 1 mm."
+  ]
 
     var factData = facts[Math.floor(Math.random()*facts.length)];
 
-  } //getFact
+    res.status(200).send('Tardigrade Fact' + factData);
 
-
-  // function postToSlack(message, info, cb){
-  //   request.post('https://slack.com/api/chat.postMessage', {
-  //     form: {
-  //       token: info.slackToken,
-  //       channel: info.channel,
-  //       text: message,
-  //       username: 'Mr. Tardigrade',
-  //       icon_url: 'https://i0.wp.com/www.heardisland.org/HD_images/HD_logos/donors/3664_150a%20256.jpg'
-  //     }
-  //   }, cb);
-  //   // Use: postAsUser(user, message, { token, channel }, () => res.status(200).send('haha ur so funny'));
-  // } //postToSlack
 }
